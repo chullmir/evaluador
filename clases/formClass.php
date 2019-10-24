c8d51ab257ae30de53225929b6f1b951c36bec26
chullmir <paulochullmir@gmail.com>
2019-10-24 18:04:37 -0300

    inicio de promedios de distintas plantillas

--- clases/formClass.php
+++ clases/formClass.php
@@ -203,8 +203,62 @@ class formClass{
 			$datos['rigurosidad2'] = 0;
 		}
 		return $datos;
-		// return $data;
 	}
+
+	// public function promediosPlantilla1()
+	// {
+	// 	$db = getDB();
+	// 	$stmt = $db->prepare("SELECT * FROM evaluacion_redactores WHERE quarter = :quarter");
+	// 	$quarter = ceil(date('m') / 4);
+	// 	$stmt->bindParam("quarter",$quarter);
+	// 	$stmt->execute();
+	// 	$count = $stmt->rowCount();
+	// 	$data = $stmt->fetchAll();
+	// 	if ($count>0) {
+	// 		$contador = 0;
+	// 		$actitud1 = 0;
+	// 		$actitud2 = 0;
+	// 		$actitud3 = 0;
+	// 		$redaccion1 = 0;
+	// 		$redaccion2 = 0;
+	// 		$redaccion3 = 0;
+	// 		$redaccion4 = 0;
+	// 		$rigurosidad1 = 0;
+	// 		$rigurosidad2 = 0;
+	// 		foreach ($data as $datum) {
+	// 			$actitud1 = $actitud1 + $datum['actitud1'];
+	// 			$actitud2 = $actitud2 + $datum['actitud2'];
+	// 			$actitud3 = $actitud3 + $datum['actitud3'];
+	// 			$redaccion1 = $redaccion1 + $datum['redaccion1'];
+	// 			$redaccion2 = $redaccion2 + $datum['redaccion2'];
+	// 			$redaccion3 = $redaccion3 + $datum['redaccion3'];
+	// 			$redaccion4 = $redaccion4 + $datum['redaccion4'];
+	// 			$rigurosidad1 = $rigurosidad1 + $datum['rigurosidad1'];
+	// 			$rigurosidad2 = $rigurosidad2 + $datum['rigurosidad2'];
+	// 			$contador = $contador + 1;
+	// 		}
+	// 		$datos['actitud1'] = $actitud1 / $contador;
+	// 		$datos['actitud2'] = $actitud2 / $contador;
+	// 		$datos['actitud3'] = $actitud3 / $contador;
+	// 		$datos['redaccion1'] = $redaccion1 / $contador;
+	// 		$datos['redaccion2'] = $redaccion2 / $contador;
+	// 		$datos['redaccion3'] = $redaccion3 / $contador;
+	// 		$datos['redaccion4'] = $redaccion4 / $contador;
+	// 		$datos['rigurosidad1'] = $rigurosidad1 / $contador;
+	// 		$datos['rigurosidad2'] = $rigurosidad2 / $contador;
+	// 	} else {
+	// 		$datos['actitud1'] = 0;
+	// 		$datos['actitud2'] = 0;
+	// 		$datos['actitud3'] = 0;
+	// 		$datos['redaccion1'] = 0;
+	// 		$datos['redaccion2'] = 0;
+	// 		$datos['redaccion3'] = 0;
+	// 		$datos['redaccion4'] = 0;
+	// 		$datos['rigurosidad1'] = 0;
+	// 		$datos['rigurosidad2'] = 0;
+	// 	}
+	// 	return $datos;
+	// }
 
 
 	public function storeEncuestaPlantilla1($POST)
